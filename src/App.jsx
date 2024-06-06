6a6556e409ed483b2ee9b171fb3c6367f8d7ffee
Jensone <hello@jensone.com>
2024-06-06 23:01:57 +0200

    ✨ App.jsx: refactoriser le contenu de la fonction start pour afficher un message plus concis et modifier le style du composant principal pour inclure une image de fond 🔧 style.css: ajouter un fichier de style pour définir l'image de fond du composant principal ♻️ main.jsx: commenter l'import du fichier de style pour éviter les conflits de style

--- src/App.jsx
+++ src/App.jsx
@@ -5,47 +5,39 @@ import loading from "../public/sounds/loading.mp3";
 function App() {
   const [instruction, setInstruction] = useState("Click on START to begin.");
   const audio = new Audio(loading);
-  const [btn, setBtn] = useState("bg-blue-500 hover:bg-blue-700");
-  const [btnText, setBtnText] = useState("START");
 
   const start = () => {
     audio.play();
     setTimeout(() => {
-      setInstruction(`
-        <p>Supakar est un petit projet pour l'apprentissage de React. </p>
-          <p>Il est basé sur le template Vite + React. </p>
-          <p>Pour démarrer, il suffit de cliquer sur le boutton "START". </p>
-          <p>Bonne session de codage !</p>
-        `);
+      setInstruction(
+        'Supakar est un projet pour l\'apprentissage de React. Il est basé sur le template Vite + React. Pour démarrer, il suffit de lire le fichier "README.md". Bonne session de codage !',
+      );
     }, 1300);
-    setBtn("bg-red-500 hover:bg-red-700");
-    setBtnText("STOP");
   };
 
   return (
     <>
-      <div className="p-5 flex flex-col items-center justify-center h-screen">
-        <div className="w-64 mb-10">
-          <img src={supakarLogo} alt="Supakar Logo" />
-        </div>
-        <button
-          onClick={
-            btnText === "START"
-              ? start
-              : () => {
-                  audio.pause();
-                  audio.currentTime = 0;
-                  setInstruction("Click on START to begin.");
-                  setBtn("bg-blue-500 hover:bg-blue-700");
-                  setBtnText("START");
-                }
-          }
-          className={btn + " text-white font-bold py-2 px-4 rounded"}
-        >
-          {btnText}
-        </button>
-        <div className="text-center text-lg transitison duration-300">
-          {instruction}
+      <div
+        className="p-5 flex flex-col items-center justify-center h-screen"
+        style={{
+          backgroundImage: "url('/cars/gray.png')",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="p-10 flex flex-col items-center justify-center shadow-2xl rounded-2xl w-96 bg-white bg-opacity-70">
+          <div className="w-64 mb-10">
+            <img src={supakarLogo} alt="Supakar Logo" />
+          </div>
+          <button
+            onClick={start}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-5 rounded"
+          >
+            START
+          </button>
+          <div className="text-center text-lg transitison duration-300">
+            {instruction}
+          </div>
         </div>
       </div>
     </>
