51757574682c02743456daaebdcc6485f35db43d
NDEBY Véronique Chimène <80843218+Shimouna-14@users.noreply.github.com>
2024-10-31 14:49:17 +0100

    Mise en commentaire des éléments non utilisées

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import supakarLogo from "../public/logo.png";
 import loading from "../public/sounds/loading.mp3";
 
 function App() {
-  const [instruction, setInstruction] = useState("Click on START to begin.");
-  const audio = new Audio(loading);
+  // const [instruction, setInstruction] = useState("Click on START to begin.");
+  // const audio = new Audio(loading);
 
-  const start = () => {
-    audio.play();
-    setTimeout(() => {
-      setInstruction(
-        'Supakar est un projet pour l\'apprentissage de React. Il est basé sur le template Vite + React. Pour démarrer, il suffit de lire le fichier "README.md". Bonne session de codage !',
-      );
-    }, 1300);
-  };
+  // const start = () => {
+  //   audio.play();
+  //   setTimeout(() => {
+  //     setInstruction(
+  //       'Supakar est un projet pour l\'apprentissage de React. Il est basé sur le template Vite + React. Pour démarrer, il suffit de lire le fichier "README.md". Bonne session de codage !',
+  //     );
+  //   }, 1300);
+  // };
 
   return (
     <>
